ae256a2c5ea4c2382b7e18ad80cee2513e8d55c1
unknown <baelanche@naver.com>
2024-04-12 22:00:55 +0900

    스도쿠 레이아웃, 숫자 출력

--- src/js/routes/SudokuView.js
+++ src/js/routes/SudokuView.js
@@ -1,31 +1,26 @@
 import Component from "../classes/Component";
-import sudoku from "../logics/Sudoku";
 import { SudokuDifficultyButton } from '../components/sudoku/SudokuDifficultyButton';
+import { SudokuGameScreen } from "../components/sudoku/SudokuGameScreen";
 
 export class SudokuView extends Component {
     constructor($parent) {
         super($parent, 'div');
     }
 
-    getSudoku() {
-        /**
-         * easy : 36~46
-         * medium : 32~35
-         * hard : 28~31
-         * evil : 17~27
-         */
-        return sudoku.generate("easy")
-    }
-
     render() {
-        const sudoku = this.getSudoku();
-        console.log(sudoku);
+        const buttons = document.createElement('div');
+        buttons.className = 'd-flex justify-content-center';
+        new SudokuDifficultyButton(buttons, { 'class': 'buttonicon difficulty' }, { 'text': '쉬움' });
+        new SudokuDifficultyButton(buttons, { 'class': 'buttonicon difficulty' }, { 'text': '보통' });
+        new SudokuDifficultyButton(buttons, { 'class': 'buttonicon difficulty' }, { 'text': '어려움' });
+        new SudokuDifficultyButton(buttons, { 'class': 'buttonicon difficulty' }, { 'text': '짱어려움' });
+        new SudokuDifficultyButton(buttons, { 'class': 'buttonicon difficulty' }, { 'text': '짱짱어려움' });
+        new SudokuDifficultyButton(buttons, { 'class': 'buttonicon difficulty' }, { 'text': '짱짱짱어려움' });
+        this.$self.appendChild(buttons);
 
-        const div = document.createElement('div');
-        div.className = 'd-flex justify-content-center';
-        new SudokuDifficultyButton(div, { 'class': 'buttonicon difficulty' }, { 'text': '쉬움' });
-        new SudokuDifficultyButton(div, { 'class': 'buttonicon difficulty' }, { 'text': '보통' });
-        new SudokuDifficultyButton(div, { 'class': 'buttonicon difficulty' }, { 'text': '어려움' });
-        this.$self.appendChild(div);
+        const screen = document.createElement('div');
+        screen.className = 'd-flex justify-content-center sudoku-container';
+        new SudokuGameScreen(screen);
+        this.$self.appendChild(screen);
     }
 }
